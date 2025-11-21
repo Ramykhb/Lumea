15ea4df24616463a4cc109d49b52ab4b775c34ca
Ramy Khachab <ramykhb18@gmail.com>
2025-11-21 21:52:27 +0200

    Added custom domain to front path

--- server/config/frontConfig.js
+++ server/config/frontConfig.js
@@ -1,3 +1,4 @@
 // export const frontendPath = "http://localhost:5173";
-export const frontendPath = "https://lumea-alpha.vercel.app";
+// export const frontendPath = "https://lumea-alpha.vercel.app";
+export const frontendPath = "https://lumea-lb.site";
 // export const frontendPath = "https://b1hqqjqw-5173.euw.devtunnels.ms";
