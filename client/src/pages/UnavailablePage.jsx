8c0a0914c604206ff1a251d871d44c1b802f8337
Ramy Khachab <ramykhb18@gmail.com>
2025-11-25 19:48:36 +0200

    Color fix

--- client/src/pages/UnavailablePage.jsx
+++ client/src/pages/UnavailablePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UnavailablePage = (props) => {
     return (
         <div className="w-full h-[100vh] flex items-center justify-center bg-primary-light dark:bg-primary-dark">
-            <div className="text-center flex flex-col items-center gap-4 p-8 bg-white/40 dark:bg-black/40 rounded-xl backdrop-blur-md shadow-lg">
+            <div className="text-center flex flex-col items-center gap-4 p-8 bg-white/40 dark:bg-black/40 rounded-xl backdrop-blur-md shadow-lg dark:text-white">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="w-20 h-20"
@@ -18,10 +18,10 @@ const UnavailablePage = (props) => {
                         d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
                     />
                 </svg>
-                <h1 className="text-3xl font-semibold">
+                <h1 className="text-3xl font-semibold dark:text-white">
                     Lumea Currently Unavailable
                 </h1>
-                <p className="text-lg opacity-70 max-w-md">
+                <p className="text-lg opacity-70 max-w-md dark:text-white">
                     Sorry Lumea is currently Unavailable.
                 </p>
             </div>
